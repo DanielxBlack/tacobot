8354f63fbd0750eb8decd3e24cbf6d683e7545d8
Daniel <DanielxBlack@users.noreply.github.com>
2019-09-29 17:01:17 -0500

    Updated Taco bot
    updated

--- tacobot.py
+++ tacobot.py
@@ -21,6 +21,7 @@ twitter = Twython(consumer_key, consumer_secret, access_token,access_token_secre
 
 
 
+
 # Below is the script that will power the taco bot.
 
 # This will use datetime to get the weekday.
@@ -34,8 +35,139 @@ def getweekday():
         message = "It's Taco Tuesday!"
     else:
         message = f"It's {weekday}, meaning it's NOT Taco Tuesday."
-    twitter.update_status(status=message)
-    return getweekday
+        twitter.update_status(status=message)
+        return getweekday
+
+
+
+# Search for taco tweets and reply.
+def searchTacos():
+    ids_replied_to = []
+    with open('ids_replied_to.txt', 'r') as filehandle:
+        filecontents = filehandle.readlines()
+
+        for line in filecontents:
+            # remove linebreak which is the last character of the string
+            current_place = line[:-1]
+            # add item to the list
+            ids_replied_to.append(current_place)
+
+    search_term = "tacos"
+    print("Searching Twitter for 'tacos'...")
+    results = twitter.cursor(twitter.search, q=search_term)
+
+
+
+    for result in results:
+        name = result["user"]
+        screen_name = name["screen_name"]
+        creation_date = result["created_at"]
+        tweet_text = result["text"]
+        id = result["id"]
+
+        # post the tweet
+        id = str(id)
+        if id in ids_replied_to:
+            print("skipped as already replied to.")
+            print()
+            print()
+        else:
+            twitter_handle = f"@{screen_name}"
+            message = f"{twitter_handle} Hey! I see you're talking about tacos. I like tacos, too!"
+            twitter.update_status(status=message, in_reply_to_status_id=id)
+            print("Tweeted: %s" % message)
+            id = int(id)
+            ids_replied_to.append(id)
+            with open("ids_replied_to.txt", "w") as filehandle:
+                filehandle.writelines("%s\n" % place for place in ids_replied_to)
+                # delay so that it doesn't look like the program is spamming Twitter
+
+
+def fkTacos():
+    ids_replied_to = []
+    with open('ids_replied_to.txt', 'r') as filehandle:
+        filecontents = filehandle.readlines()
+
+        for line in filecontents:
+            # remove linebreak which is the last character of the string
+            current_place = line[:-1]
+            # add item to the list
+            ids_replied_to.append(current_place)
+
+    search_term = "fuck tacos"
+    print("Searching Twitter for 'fuck tacos'...")
+    results = twitter.cursor(twitter.search, q=search_term)
+
+
+
+    for result in results:
+        name = result["user"]
+        screen_name = name["screen_name"]
+        creation_date = result["created_at"]
+        tweet_text = result["text"]
+        id = result["id"]
+
+        # post the tweet
+        id = str(id)
+        if id in ids_replied_to:
+            print("skipped as already replied to.")
+            print()
+            print()
+        else:
+            twitter_handle = f"@{screen_name}"
+            message = f"{twitter_handle} I got your tacos right here! *grabs crotch and throws you the finger*"
+            twitter.update_status(status=message, in_reply_to_status_id=id)
+            print("Tweeted: %s" % message)
+            id = int(id)
+            ids_replied_to.append(id)
+            with open("ids_replied_to.txt", "w") as filehandle:
+                filehandle.writelines("%s\n" % place for place in ids_replied_to)
+                # delay so that it doesn't look like the program is spamming Twitter
+
+
+
+# Search for taco tweets and reply.
+def searchNonTacos():
+    ids_replied_to = []
+    with open('ids_replied_to.txt', 'r') as filehandle:
+        filecontents = filehandle.readlines()
+
+        for line in filecontents:
+            # remove linebreak which is the last character of the string
+            current_place = line[:-1]
+            # add item to the list
+            ids_replied_to.append(current_place)
+
+    search_terms = ["hamburger", "burger", "hotdog", "pizza", "sandwich"]
+    randomFood = random.choice(search_terms)
+    print(f"Searching Twitter for {randomFood} ...")
+    results = twitter.cursor(twitter.search, q=randomFood)
+
+
+
+    for result in results:
+        name = result["user"]
+        screen_name = name["screen_name"]
+        creation_date = result["created_at"]
+        tweet_text = result["text"]
+        id = result["id"]
+
+        # post the tweet
+        id = str(id)
+        if id in ids_replied_to:
+            print("skipped as already replied to.")
+            print()
+            print()
+        else:
+            twitter_handle = f"@{screen_name}"
+            message = f"{twitter_handle} I mean, {randomFood} is cool, but have you considered tacos instead? Just an idea."
+            twitter.update_status(status=message, in_reply_to_status_id=id)
+            print("Tweeted: %s" % message)
+            id = int(id)
+            ids_replied_to.append(id)
+            with open("ids_replied_to.txt", "w") as filehandle:
+                filehandle.writelines("%s\n" % place for place in ids_replied_to)
+
 
 
 # This will get a random quote from a list of quotes and print it.
@@ -43,6 +175,7 @@ def getRandomQuote():
     quotes = open('tacoquotes.txt').read().splitlines()
     todaysQuote = random.choice(quotes)
     message = todaysQuote
+    twitter.update_status(status=message)
     return getRandomQuote
 
 
@@ -56,24 +189,14 @@ def getCityTacos():
     return getCityTacos
 
 """
-def searchTacos():
-    print("I see you're talking about tacos. I like tacos.")
-    This is still in the works. It will search twitter for taco tweets and
-    it will reply to the tweet with "I see you're talking about tacos."
-
-
 def searchPizza():
     foodToSearch = [pizza, steak, cookies]
     aFood = random.choice(foodToSearch)
     print(f"I see you're talking about {aFood}. That is cool, but tacos are best. IMO.")
-
-
 def searchAFood():
     foodToSearch = ["pizza", "steak", "cookies", "burgers"]
     aFood = random.choice(foodToSearch)
     print(f"I see you're talking about {aFood}. That is cool, but tacos are best. IMO.")
-
-
 """
 
 
@@ -81,20 +204,27 @@ def searchAFood():
 # of functions that print taco things.
 
 def tacobotAction():
-    tacobotActions = [getweekday, getRandomQuote, getCityTacos]
+    tacobotActions = [getRandomQuote, getRandomQuote, getRandomQuote,
+    getweekday, getRandomQuote, getRandomQuote, getRandomQuote,
+    getRandomQuote, getCityTacos, getRandomQuote, getRandomQuote,
+    getRandomQuote]
     random.choice(tacobotActions)()
     # Set tweeting times to long, random intervals
-    whenToTweet = [10800, 3600, 7200, 21600, 16200, 8640, 18360] # Random interval for tweets.
+    whenToTweet = [3600, 7200, 10800, 18000, 7380,
+    14400, 18600, 5400, 21600] # Random interval for tweets.
     whenToTweet = random.choice(whenToTweet)
+    print(f" TacoBot will pause for {whenToTweet} seconds before tweeting again.")
     time.sleep(whenToTweet) # Pause for a random number of seconds (hours) then tweet again.
     return tacobotAction
 
 
 
 while True:
+    gettheDate = datetime.datetime.now()
+    rightNow = gettheDate.strftime("%x")
+    print(f"Tacobot ran at {gettheDate}.")
+
     tacobotAction()
-
-
 
 
 
@@ -105,4 +235,3 @@ while True:
 # To to:
 # -- Figure out hattrick - Eating tacos for breakfast, lunch and dinner in
 #    a single day will create hattrick.
-# -- Search taco tweets. Will reply "Hey. I see you're talking about tacos."
