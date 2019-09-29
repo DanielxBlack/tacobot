051cb15213b38b6ea9a77395f80ee817c9f9dd71
Daniel <DanielxBlack@users.noreply.github.com>
2019-09-28 19:34:06 -0500

    Small Update
    Small update. Using twython. Will probably move to Twitter API later. I just want to get this thing running.

--- tacobot.py
+++ tacobot.py
@@ -5,21 +5,27 @@ import datetime
 import json
 import time
 import random
+import sys
+
 
 # Third-party Python libraries.
 import requests
 import TwitterSearch
+from twython import Twython
 
 # Custom Python libraries.
+# Import API Keys for Auth
+from auth import (consumer_key, consumer_secret, access_token, access_token_secret)
+
+twitter = Twython(consumer_key, consumer_secret, access_token,access_token_secret)
+
 
 
 # Below is the script that will power the taco bot.
 
-
-
-
 # This will use datetime to get the weekday.
 # If it's Tuesday, it means Taco Tuesday.
+
 def getweekday():
     week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
     dayofweek = datetime.datetime.today().weekday()
@@ -28,6 +34,7 @@ def getweekday():
         print("It's Taco Tuesday!")
     else:
         print(f"It's {weekday}, meaning it's NOT Taco Tuesday.")
+    return getweekday
 
 
 # This will get a random quote from a list of quotes and print it.
@@ -35,6 +42,7 @@ def getRandomQuote():
     quotes = open('tacoquotes.txt').read().splitlines()
     todaysQuote = random.choice(quotes)
     print(todaysQuote)
+    return getRandomQuote
 
 
 # This will ask where to get tacos in a city pulled randomly from a list
@@ -42,7 +50,8 @@ def getRandomQuote():
 def getCityTacos():
     cityTacos = open('uscities.txt').read().splitlines()
     tacosinthecity = random.choice(cityTacos)
-    print(f"Does anyone know where I can get good tacos in {tacosinthecity}?")
+    #print(f"Does anyone know where I can get good tacos in {tacosinthecity}?")
+    return getCityTacos
 
 """
 def searchTacos():
@@ -58,20 +67,32 @@ def searchTacos():
 
 def tacobotAction():
     tacobotActions = [getweekday, getRandomQuote, getCityTacos]
-    #whenToTweet = [10800, 3600, 7200, 21600, 16200, 8640, 18360]
-    whenToTweet = [1,2,3] # only for testing
-    tacoTweet = random.choice(tacobotActions)()
+    random.choice(tacobotActions)()
+    # Set tweeting times to long, random intervals
+    #whenToTweet = [10800, 3600, 7200, 21600, 16200, 8640, 18360] # Random interval for tweets.
+    whenToTweet = [9,5]
     whenToTweet = random.choice(whenToTweet)
-    time.sleep(whenToTweet)
-
-
-
-while True:
-    tacobotAction()
+    time.sleep(whenToTweet) # Pause for a random number of seconds (hours) then tweet again.
 
 
 
 
+
+
+
+"""
+
+    #twitter.update_status(status=)
+
+"""
+
+
+
+
+
+
+#tacobotAction()
+#twitter.update_status(status=)
 
 # To to:
 # -- Figure out hattrick - Eating tacos for breakfast, lunch and dinner in
